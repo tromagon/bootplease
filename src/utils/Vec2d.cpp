5f61c6f63cfaa7e48f77d7e3790555aa9a73c8a0
Thomas Barembaum <thomas.barembaum@gmail.com>
2015-01-07 15:47:09 -0500

    float instead of int in Vec2d

--- src/utils/Vec2d.cpp
+++ src/utils/Vec2d.cpp
@@ -11,7 +11,18 @@ Vec2d::Vec2d(int x, int y)
 	Set(x, y);
 }
 
+Vec2d::Vec2d(float x, float y)
+{
+	Set(x, y);
+}
+
 void Vec2d::Set(int x, int y)
+{
+	this->x = x;
+	this->y = y;
+}
+
+void Vec2d::Set(float x, float y)
 {
 	this->x = x;
 	this->y = y;
--- src/utils/Vec2d.h
+++ src/utils/Vec2d.h
@@ -4,14 +4,16 @@
 class Vec2d
 {
 public:
-	int x, y;
+	float x, y;
 
 public:
 	Vec2d();
-	explicit Vec2d(int x, int y );
+	explicit Vec2d(int x, int y);
+	explicit Vec2d(float x, float y);
 	~Vec2d() {}
 
 	void	Set(int x, int y);
+	void	Set(float x, float y);
 };
 
 #endif

3a99c21a0c4f7fc985d695c58d1e8c31686e5cbf
Thomas Barembaum <thomas.barembaum@gmail.com>
2015-02-08 10:52:53 -0500

    test auto format

--- src/sequence/Sequence.h
+++ src/sequence/Sequence.h
@@ -10,40 +10,40 @@ using namespace std;
 class Sequence : public ISequence
 {
 public:
-	explicit Sequence(EventDispatcherPtr& dispatcher);
+    explicit Sequence(EventDispatcherPtr& dispatcher);
     ~Sequence() {};
 
-	virtual int		IncrementIndex() override;
-	virtual void	CompleteStep() override;
-	virtual bool	IsNextStepAsync() override;
+    virtual int		IncrementIndex() override;
+    virtual void	CompleteStep() override;
+    virtual bool	IsNextStepAsync() override;
 
-	void			Start();
+    void			Start();
     void            Dispatch(const Event& evt);
-	void			Dispatch(const Event& evt, EventDispatcherPtr& dispatcher);
+    void			Dispatch(const Event& evt, EventDispatcherPtr& dispatcher);
     void			WaitFor(const char* eventType);
-	void			WaitFor(const char* eventType, EventDispatcherPtr& dispatcher);
+    void			WaitFor(const char* eventType, EventDispatcherPtr& dispatcher);
 
-	template<class C>
-	void			Call(void (C::*fct)(), C& proxy);
     template<class C>
-	void			WaitFor(const char* eventType, void (C::*fct)(const Event&), C& proxy);
-	template<class C>
-	void			WaitFor(const char* eventType, void (C::*fct)(const Event&), C& proxy, EventDispatcherPtr& dispatcher);
+    void			Call(void (C::*fct)(), C& proxy);
+    template<class C>
+    void			WaitFor(const char* eventType, void (C::*fct)(const Event&), C& proxy);
+    template<class C>
+    void			WaitFor(const char* eventType, void (C::*fct)(const Event&), C& proxy, EventDispatcherPtr& dispatcher);
 
 private:
-	void			NextStep();
+    void			NextStep();
 
-	EventDispatcherPtr&			m_Dispatcher;
-	vector<StepPtr>				m_List;
-	int							m_CurrentIndex;
-	int							m_NumSteps;
+    EventDispatcherPtr&			m_Dispatcher;
+    vector<StepPtr>				m_List;
+    int							m_CurrentIndex;
+    int							m_NumSteps;
 };
 
 template<class C>
 void Sequence::Call(void (C::*fct)(), C& proxy)
 {
-	StepPtr step = StepPtr(new CallStep<C>(*this, fct, proxy));
-	m_List.push_back(move(step));
+    StepPtr step = StepPtr(new CallStep<C>(*this, fct, proxy));
+    m_List.push_back(move(step));
     m_NumSteps++;
 }
 
@@ -58,7 +58,7 @@ void Sequence::WaitFor(const char* eventType, void (C::*fct)(const Event&), C& p
 template<class C>
 void Sequence::WaitFor(const char* eventType, void (C::*fct)(const Event&), C& proxy, EventDispatcherPtr& dispatcher)
 {
-	StepPtr step = StepPtr(new WaitForEventStep<C>(*this, eventType, fct, proxy, dispatcher));
+    StepPtr step = StepPtr(new WaitForEventStep<C>(*this, eventType, fct, proxy, dispatcher));
     m_List.push_back(move(step));
     m_NumSteps++;
 }

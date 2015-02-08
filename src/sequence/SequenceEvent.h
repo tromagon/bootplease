ba8654f6a2f7c812eb3dc88def1b3eb6c8ea2b8c
Thomas Barembaum <thomas.barembaum@gmail.com>
2015-02-08 11:06:35 -0500

    auto format

--- src/sequence/SequenceEvent.h
+++ src/sequence/SequenceEvent.h
@@ -8,19 +8,19 @@ class Sequence;
 class SequenceEvent : public Event
 {
 public:
-	static const char* STARTED;
-	static const char* COMPLETE;
-	static const char* STEP_COMPLETE;
+    static const char* STARTED;
+    static const char* COMPLETE;
+    static const char* STEP_COMPLETE;
 
 private:
-	Sequence& m_Sequence;
+    Sequence& m_Sequence;
 
 public:
-	Sequence&	GetSequence() const	{ return m_Sequence; }
+    Sequence&   GetSequence() const { return m_Sequence; }
 
 public:
-	explicit SequenceEvent(const char* type, Sequence& sequence) : Event(type), m_Sequence(sequence) {}
-	virtual ~SequenceEvent() {};
+    explicit SequenceEvent(const char* type, Sequence& sequence) : Event(type), m_Sequence(sequence) {}
+    virtual ~SequenceEvent() {};
 };
 
 #endif

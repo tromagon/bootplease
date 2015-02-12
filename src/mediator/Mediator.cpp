122bb8a262bd1e61a9ad0179d1bb46bf231c4aa9
Thomas Barembaum <thomas.barembaum@gmail.com>
2015-02-11 19:57:52 -0500

    clean up

--- src/mediator/Mediator.cpp
+++ src/mediator/Mediator.cpp
@@ -1,6 +1,17 @@
 #include "Mediator.h"
+#include "mediator\MediatorMap.h"
 
 void Mediator::DispatchContextEvent(const Event& evt)
 {
     GetDispatcher()->Dispatch(evt);
+}
+
+InjectorPtr& Mediator::GetInjector()       
+{ 
+    return m_MediatorMap->GetInjector(); 
+} 
+
+EventDispatcherPtr& Mediator::GetDispatcher()     
+{ 
+    return m_MediatorMap->GetDispatcher(); 
 }
